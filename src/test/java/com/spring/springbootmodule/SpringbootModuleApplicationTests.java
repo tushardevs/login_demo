6ab991a7937afdcccf91b500845e8fb701adf1b3
tushardevs <tusharbhatnagar053@gmail.com>
2021-03-19 21:42:46 +0530

    Initial commit

---
+++ src/test/java/com/spring/springbootmodule/SpringbootModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
